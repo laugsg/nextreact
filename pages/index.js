d7ad171c62c10432717bf96b8f8d52532d7c8f27
laugsg <lautaro.grucci@joingsg.com>
2022-06-13 00:24:03 +0200

    update > collapsible boxes (accordion)

--- pages/index.js
+++ pages/index.js
@@ -22,60 +22,57 @@ import { themeDefaultButton } from "../vanilla-styles/themes/theme-button-defaul
 import { gridBox } from "../vanilla-styles/components/markup/recipe-accordion.css";
 // components
 import { Navigation } from "../vanilla-styles/components/markup/Navigation";
-import { Accordion, MultiAccordion } from "../vanilla-styles/components/markup/Accordion";
-
+import { MultiAccordion } from "../vanilla-styles/components/markup/Accordion";
 
 export default function HomePage() {
-  const firstCard =[
-      {
-        title: 'Client-Side "See More"',
-        para: 'This card implements "🛒  See More" button as the base style and produce two variants from styleVariant() which are implemented in the following cards.'
-      },
-      {
-        title: 'Server-Side "Add to Cart"',
-        para: 'This card implements SSR button "Add to Cart", It\'s based in a contract/theme create with client side API but I\'s implemented using Sprinkles to provide it from SSR. Which will be taken as a base to following variants with recipes API.'
-      },
-      {
-        title: 'Client-Side Badge',
-        para:'The badge as "Discount" reproduce client side rendering simple and slim as a synthesis of the styles and variants produced for client side buttons, where to be explicit, differents contracts and values were usually mixed to explore the very edges of the differents APIs that vanilla-extract offers.'
-      },
-    ];
+  const firstCard = [
+    {
+      title: 'Client-Side "See More"',
+      para: 'This card implements "🛒  See More" button as the base style and produce two variants from styleVariant() which are implemented in the following cards.',
+    },
+    {
+      title: 'Server-Side "Add to Cart"',
+      para: "This card implements SSR button \"Add to Cart\", It's based in a contract/theme create with client side API but I's implemented using Sprinkles to provide it from SSR. Which will be taken as a base to following variants with recipes API.",
+    },
+    {
+      title: "Client-Side Badge",
+      para: 'The badge as "Discount" reproduce client side rendering simple and slim as a synthesis of the styles and variants produced for client side buttons, where to be explicit, differents contracts and values were usually mixed to explore the very edges of the differents APIs that vanilla-extract offers.',
+    },
+  ];
 
-    const secondCard = [
-      {
-        title: '"See More" variant (client side)',
-        para:'This button has base styles that are replaced by the implementation of a contract that is reused to extract values and to combine them with new ones provided. It reuses styles as margin, from the base style, introduce a reused padding and replace values as border-radius. The final result is an element that recycles a contract and a theme previously built over which extend the element styles with own values.'
-      },
-      {
-        title: '"Add to Cart" as recycling',
-        para:'The base style were setup to be share by this and next variant, reuses two contracts/themes from client side which implements in server side and shares a Sprinkles which even reuse a client side contract that implements at the same time with new values though the server side too.'
-      },
-      {
-        title: '"Add to Cart" as variant',
-        para:'As a variant, this element uses Recipe API which reuses three contracts and combine this with Sprinkles. It produces a variant based in combined properties.'
-      },
-      {
-        title: 'Client-Side Badge',
-        para:'Simple and slim, this badge is a client side badge variant that reuses a contract/theme, uses base style for the element and introduce its own values.'
-      },
-    ];
+  const secondCard = [
+    {
+      title: '"See More" variant (client side)',
+      para: "This button has base styles that are replaced by the implementation of a contract that is reused to extract values and to combine them with new ones provided. It reuses styles as margin, from the base style, introduce a reused padding and replace values as border-radius. The final result is an element that recycles a contract and a theme previously built over which extend the element styles with own values.",
+    },
+    {
+      title: '"Add to Cart" as recycling',
+      para: "The base style were setup to be share by this and next variant, reuses two contracts/themes from client side which implements in server side and shares a Sprinkles which even reuse a client side contract that implements at the same time with new values though the server side too.",
+    },
+    {
+      title: '"Add to Cart" as variant',
+      para: "As a variant, this element uses Recipe API which reuses three contracts and combine this with Sprinkles. It produces a variant based in combined properties.",
+    },
+    {
+      title: "Client-Side Badge",
+      para: "Simple and slim, this badge is a client side badge variant that reuses a contract/theme, uses base style for the element and introduce its own values.",
+    },
+  ];
 
-
-
-    const thirdCard = [
-      {
-        title: '"See More" variant (client side)',
-        para:'This variant of the element uses the base styles, the base theme styles and introduce its own values. The final result is an element that reuses styles created on the fly, contract and themes previously defined and carring with all those rules add their own specific ones.'
-      },
-      {
-        title: '"Add to Cart" as variant "on the fly"',
-        para:'Through Recipe API were defined standard variants, then it was compound a combination of values which result in a new variant defined on the fly that includes the reuse of a contract/theme.'
-      },
-      {
-        title: 'Client-Side Badge',
-        para:'This variant implements isolated styles without use or reuse contracts, themes or base element styles, introduce its own values providing them as an alternative.'
-      },
-    ];
+  const thirdCard = [
+    {
+      title: '"See More" variant (client side)',
+      para: "This variant of the element uses the base styles, the base theme styles and introduce its own values. The final result is an element that reuses styles created on the fly, contract and themes previously defined and carring with all those rules add their own specific ones.",
+    },
+    {
+      title: '"Add to Cart" as variant "on the fly"',
+      para: "Through Recipe API were defined standard variants, then it was compound a combination of values which result in a new variant defined on the fly that includes the reuse of a contract/theme.",
+    },
+    {
+      title: "Client-Side Badge",
+      para: "This variant implements isolated styles without use or reuse contracts, themes or base element styles, introduce its own values providing them as an alternative.",
+    },
+  ];
 
   return (
     <>
@@ -91,14 +88,12 @@ export default function HomePage() {
             <ButtonOne />
             <ButtonOneVariant />
           </Card>
-          
 
           <div style={{ maxWidth: "500px", minWidth: "80px" }}>
-          <MultiAccordion items={firstCard} />
+            <MultiAccordion items={firstCard} />
           </div>
         </div>
       </div>
-      
 
       <br />
       <br />
@@ -111,7 +106,7 @@ export default function HomePage() {
           </Card>
 
           <div style={{ maxWidth: "500px", minWidth: "80px" }}>
-          <MultiAccordion items={secondCard} />
+            <MultiAccordion items={secondCard} />
           </div>
         </div>
       </div>
@@ -127,7 +122,7 @@ export default function HomePage() {
           </Card>
 
           <div style={{ maxWidth: "500px", minWidth: "80px" }}>
-          <MultiAccordion items={thirdCard} />
+            <MultiAccordion items={thirdCard} />
           </div>
         </div>
       </div>
@@ -165,7 +160,11 @@ function Header() {
             it.
           </p>
           <p style={{ margin: "0px" }}>
-            Every file in this proyect has comments and its commented for learning purposes.
+            Every file in this proyect has comments and its commented for
+            learning purposes.
+          </p>
+          <p style={{ margin: "0px", fontStyle: "italic" }}>
+            <u>Click in the boxes to know more (+ 'item title')</u>
           </p>
         </div>
       </div>
